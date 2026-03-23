130bcc2d0a11e0ca70dfc7d8deb9113bed5ed53a
Muttaqi <muntasermuttaqi@gmail.com>
2026-03-23 23:10:41 +0600

    refactor: Remove react-helmet-async
    Migrates SEO meta tag management from react-helmet-async to direct DOM manipulation for simplicity and to reduce bundle size. This change removes the dependency and associated boilerplate code.

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.tsx";
 import "./index.css";
 import { registerSW } from 'virtual:pwa-register';
@@ -10,8 +9,6 @@ registerSW({ immediate: true });
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </StrictMode>,
 );
