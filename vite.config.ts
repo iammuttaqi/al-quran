257a9a36077ada5c29c95e0029c2be79287cb6e4
Muttaqi <muntasermuttaqi@gmail.com>
2026-03-25 19:06:11 +0600

    refactor: Replace favicon.svg references with pwa-icon.svg and og-image.jpg, and delete favicon.svg.

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,18 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import {defineConfig, loadEnv} from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig(({mode}) => {
+export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
     plugins: [
-      react(), 
+      react(),
       tailwindcss(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.svg'],
+        includeAssets: ['pwa-icon.svg'],
         manifest: {
           name: 'Al Quran Digital',
           short_name: 'Al Quran',
@@ -21,7 +21,7 @@ export default defineConfig(({mode}) => {
           background_color: '#0c0a09',
           icons: [
             {
-              src: 'favicon.svg',
+              src: 'pwa-icon.svg',
               sizes: '192x192 512x512',
               type: 'image/svg+xml',
               purpose: 'any maskable'
